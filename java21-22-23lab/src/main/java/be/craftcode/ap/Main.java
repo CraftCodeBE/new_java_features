e4fa87476d59dfe9bad715125709f697b530398d
John Wilkos <john.wilkos@craftcode.be>
2024-12-06 17:16:14 +0100

    Removed string templates tests and added new hello world to main.java

--- java21-22-23lab/src/main/java/be/craftcode/ap/Main.java
+++ java21-22-23lab/src/main/java/be/craftcode/ap/Main.java
@@ -1,7 +1,17 @@
-package be.craftcode.ap;
+//Notice no package name or imports up here!
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+//***Implicitly Declared Classes and Instance Main Methods (Third Preview) – JEP 477***
+
+//Old Hello World. Public? Static? Void? Unused string args? Terrifying Java beginners since 1996:
+
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
+
+//New Hello World. Who is this young and attractive language? 30 years old? Impossible! You don't look a day over 15.
+void main(){
+    println("Hello world!");
 }
+
